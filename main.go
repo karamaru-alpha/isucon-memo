697953358bd3770120b3e7911fdd278fa5b5e111
karamaru-alpha <mrnk3078@gmail.com>
2022-08-12 20:58:08 +0900

    a

--- main.go
+++ main.go
@@ -2,42 +2,118 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
-
-	"github.com/patrickmn/go-cache"
 )
 
-type Cache[V any] struct {
-	cache *cache.Cache
+type Cacher[T any] struct {
+	Mutex sync.RWMutex
+	Cache map[string]struct {
+		Value   T
+		Expired time.Time
+	}
 }
 
-func (c *Cache[V]) Get(key string) (V, bool) {
-	v, ok := c.cache.Get(key)
-	if ok {
-		return v.(V), true
+func (c *Cacher[T]) Get(key string) (T, bool) {
+	c.Mutex.RLock()
+	cache, ok := c.Cache[key]
+	c.Mutex.RUnlock()
+	if ok && (cache.Expired.IsZero() || time.Now().Before(cache.Expired)) {
+		return cache.Value, true
 	}
-	var defaultValue V
+	var defaultValue T
 	return defaultValue, false
 }
 
-func (c *Cache[V]) Set(key string, value V, ttl time.Duration) {
-	c.cache.Set(key, value, ttl)
+func (c *Cacher[T]) Set(key string, value T, ttl time.Duration) {
+	c.Mutex.Lock()
+	var expired time.Time
+	if ttl > 0 {
+		expired = time.Now().Add(ttl)
+	}
+	c.Cache[key] = struct {
+		Value   T
+		Expired time.Time
+	}{
+		Value:   value,
+		Expired: expired,
+	}
+	c.Mutex.Unlock()
 }
 
-func (c *Cache[V]) Flush() {
-	c.cache.Flush()
+func (c *Cacher[T]) Delete(key string) {
+	c.Mutex.Lock()
+	delete(c.Cache, key)
+	c.Mutex.Unlock()
 }
 
-type User struct {
+func (c *Cacher[T]) Flush() {
+	c.Mutex.Lock()
+	c.Cache = make(map[string]struct {
+		Value   T
+		Expired time.Time
+	})
+	c.Mutex.Unlock()
+}
+
+func initCacher[T any](typ T) Cacher[*T] {
+	return Cacher[*T]{
+		Cache: make(map[string]struct {
+			Value   *T
+			Expired time.Time
+		}, 0),
+	}
+}
+
+type Thing struct {
 	Name string
 }
 
-var userCache = Cache[[]User]{
-	cache: cache.New(time.Minute*3, time.Minute*3),
-}
+var ThingCacher = initCacher(Thing{})
 
 func main() {
-	users := []User{{Name: "karamaru"}}
-	userCache.Set("users", users, time.Second*1)
-	fmt.Println(userCache.Get("users"))
+	isu := &Thing{Name: "isu"}
+	ThingCacher.Set("isu", isu, -1)
+	fmt.Println(ThingCacher.Get("isu"))
 }
+
+// ↓メソッド拡張
+//
+//type UserCacher struct {
+//	*Cacher[*User]
+//}
+//
+//func (c *UserCacher) IncrementAge(key string) {
+//	c.Mutex.Lock()
+//	cache, ok := c.Cacher.Cache[key]
+//	if !ok {
+//		c.Mutex.Unlock()
+//		return
+//	}
+//	cache.Value.Age++
+//	c.Mutex.Unlock()
+//}
+//
+//type User struct {
+//	Name string
+//	Age  int32
+//}
+//
+//var userCache = UserCacher{
+//	Cacher: &Cacher[*User]{
+//		Cache: make(map[string]struct {
+//			Value   *User
+//			Expired time.Time
+//		}, 0),
+//	},
+//}
+//
+//func main() {
+//	user := &User{Name: "karamaru"}
+//	userCache.Set("karamaru", user, -1)
+//	fmt.Println(userCache.Get("karamaru"))
+//	userCache.IncrementAge("karamaru")
+//	fmt.Println(userCache.Get("karamaru"))
+//	time.Sleep(time.Second * 3)
+//	fmt.Println(userCache.Get("karamaru"))
+//}
